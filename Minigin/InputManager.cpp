f11a325c7f3c0bdcbf2df578970c7fa38e4698f9
gaslam <44768183+gaslam@users.noreply.github.com>
2023-03-18 15:38:04 +0100

    Fix issue with ImGui

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,6 +1,6 @@
 #include <SDL.h>
 #include "InputManager.h"
-#include <backends/imgui_impl_sdl2.h>
+#include <backends/imgui_impl_sdl.h>
 
 bool dae::InputManager::ProcessInput()
 {
--- Minigin/Renderer.cpp
+++ Minigin/Renderer.cpp
@@ -3,7 +3,7 @@
 #include "SceneManager.h"
 #include "Texture2D.h"
 #include <imgui.h>
-#include <backends/imgui_impl_sdl2.h>
+#include <backends/imgui_impl_sdl.h>
 #include <backends/imgui_impl_opengl2.h>
 
 int GetOpenGLDriverIndex()
